b4a0436969e9d5f443af7a0cd6c7936be3caa30f
hossein1376 <hosseinyazdani1376@gmail.com>
2022-07-18 20:14:33 +0430

    CSS and Style

--- yummyfingerfood/settings.py
+++ yummyfingerfood/settings.py
@@ -92,8 +92,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '.static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('.static'))]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
