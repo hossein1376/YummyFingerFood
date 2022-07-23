d594c4927c8886c01b5c1e36da6e15e46717534f
hossein1376 <hosseinyazdani1376@gmail.com>
2022-07-23 20:20:03 +0430

    Model inheritance

--- menu/models.py
+++ menu/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 
-class Cake(models.Model):
+class Menu(models.Model):
     title = models.CharField(max_length=300, verbose_name='عنوان')
     price = models.CharField(max_length=300, verbose_name='قیمت')
     description = models.CharField(
         max_length=300, blank=True, null=True, verbose_name='توضیحات (در صورت نیاز)')
 
+    class Meta:
+        abstract = True
+
+
+class Cake(Menu):
     class Meta:
         verbose_name = ('کیک')
         verbose_name_plural = ('کیک ها')
@@ -15,12 +20,7 @@ class Cake(models.Model):
         return self.title
 
 
-class Salad(models.Model):
-    title = models.CharField(max_length=300, verbose_name='عنوان')
-    price = models.CharField(max_length=300, verbose_name='قیمت')
-    description = models.CharField(
-        max_length=300, blank=True, null=True, verbose_name='توضیحات (در صورت نیاز)')
-
+class Salad(Menu):
     class Meta:
         verbose_name = ('سالاد')
         verbose_name_plural = ('سالاد ها')
