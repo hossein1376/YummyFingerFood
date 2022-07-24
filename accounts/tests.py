4b3e9d589284f1ddaf5fb541338c1fc7b630b653
hossein1376 <hosseinyazdani1376@gmail.com>
2022-07-24 12:49:28 +0430

    Accounts testcases

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class AccountsTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': '09123456789',
+            'email': 'test@email.com',
+            'password': 'secret', }
+        get_user_model().objects.create_user(**self.credentials)
+
+    def test_login(self):
+        response = self.client.login(
+            username='09123456789', password='secret',)
+        self.assertTrue(response, True)
