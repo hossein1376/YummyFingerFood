63c9ef4769b279cdfa95f54f9bdc9808105bb7ee
hossein1376 <hosseinyazdani1376@gmail.com>
2022-07-31 18:41:05 +0430

    Place order

--- orders/forms.py
+++ orders/forms.py
@@ -1,1 +1,9 @@
 from django.forms import ModelForm
+
+from .models import Order
+
+
+class OrderCreationForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = ('description',)
--- orders/urls.py
+++ orders/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from .views import order_create
+
 urlpatterns = [
+    path('new/', order_create, name='order_new'),
 ]
